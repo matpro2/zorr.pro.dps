93127b945ec4294ed12aa27251b14ae30744fcc3
matpro2 <165037110+matpro2@users.noreply.github.com>
2026-05-13 15:23:18 +0000

    commit

--- petal.js
+++ petal.js
@@ -3,8 +3,7 @@ const petals = [
     { name: "Leaf", reload: 1.0,  health: 12, damage: 15, armor: 0, },
     { name: "Trident", reload: 3.75,  health: 15, damage: 1000, armor: 5, },
     { name: "Zodiac", reload: 2.0,  health: 100, damage: 200, armor: 5, },
-    { 
-        name: "Dice",
+    {name: "Dice",
         reload: 1.0,
         health: 10,
         damage: 40,
@@ -13,6 +12,10 @@ const petals = [
             type: "luckMultiplier",
             chance: 0.08,
             multiplier: 69
-        }
-    },
+    }},
+    { name: "Heavy", reload: 7.0,  health: 1000, damage: 35, armor: 1, },
+    { name: "Rock", reload: 2.0,  health: 250, damage: 30, armor: 0, },
+    { name: "Dollar", reload: 1.0,  health: 10, damage: 30, armor: 0, },
+    { name: "Bitcoin", reload: 1.0,  health: 15, damage: 40, armor: 2, },
+    { name: "Yuan", reload: 1.0,  health: 10, damage: 20, armor: 0, },
 ];
